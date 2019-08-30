d084d99a4196e26fadb726ef9b2bf465a98a3940
Oksana <oxanakril@gmail.com>
2019-08-29 20:51:57 -0500

    some styling

--- src/NasaCard.js
+++ src/NasaCard.js
@@ -1,15 +1,29 @@
 import React from "react";
 
+
 function NewEventNASA (props){
+
     return (
         <div>
             <h3>{props.title}</h3>
-            <div className = "content-nasa">
+            {/* <div className = "content-nasa"> */}
                 <img src = {props.url} alt="" width="50%" height = "50%" />
+                <p>{props.date}</p>
                 <p>{props.explanation}</p>
-            </div>
+            {/* </div> */}
         </div>
     )
+
+// return (
+//         <div>
+//             {props.title}
+//             {/* <div className = "content-nasa"> */}
+//                 <img src = {props.url} alt="" width="50%" height = "50%" />
+//                 {props.date}
+//                 {props.explanation}
+//             {/* </div> */}
+//         </div>
+//     )
 }
 
 export default NewEventNASA;
